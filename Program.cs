2ee96aa7745bcd514c06c5689fca02d96f2ac238
Carmelo <cakye3c@student.oaisd.org>
2024-10-07 09:05:09 -0400

    finish the lab

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+        Console.WriteLine("My name is Emmanual Martinez.");
+
+        int age = 18;
+        string greeting = "Hello world";
+        string firstname = "Emmanual";
+        string lastname = "Martinez";
+        double money = 2.79;
+
+        Console.WriteLine(greeting + "! " + "My name is " + firstname + " " + lastname + ". I am " + age + "years old. I just spent $" + money + " on an Energy Drink.");
+
+    }
+}
